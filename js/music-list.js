6e7224d984e962ef8a91824f5558a93f8451230e
Rifki Rhp <kiezie08@gmail.com>
2024-12-31 21:34:01 +0700

    sample songs

---
+++ js/music-list.js
@@ -0,0 +1,92 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Apna Bana Le",
+    artist: "Arijit Singh",
+    img: "1",
+    src: "1"
+  },
+  {
+    name: "Tum Hi Ho",
+    artist: "Arijit Singh",
+    img: "2",
+    src: "2"
+  },
+  {
+    name: "Jab Tak",
+    artist: "Armaan Malik",
+    img: "3",
+    src: "3"
+  },
+  {
+    name: "Chann Sitare",
+    artist: "Ammy Virk",
+    img: "4",
+    src: "4"
+  },
+  {
+    name: "Mi Amor",
+    artist: "Sharn",
+    img: "5",
+    src: "5"
+  },
+  {
+    name: "No Love",
+    artist: "Shubh",
+    img: "6",
+    src: "6"
+  },
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "7",
+    src: "7"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "8",
+    src: "8"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "9",
+    src: "9"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "10",
+    src: "10"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "11",
+    src: "11"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "12",
+    src: "12"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
